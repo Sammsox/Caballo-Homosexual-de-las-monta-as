a8e227fc2b4af86e80a9c45bc5d72c0a00a81d89
AnderFigueroa <20220105@ricaldone.edu.sv>
2024-06-29 15:42:54 -0600

    arreglo chiquito

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,6 @@
 const HEADER = document.querySelector('header');
 HEADER.innerHTML = `
-<nav id="navbar" class="navbar navbar-expand-lg">
+<nav id="navbar" class="navbar navbar-expand-lg fixed-top">
             <div class="container-fluid">
               <a class="navbar-brand ms-4" href="#">CHDLM</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
